992672eb51088ab7d13ee5e9661399644b5219ba
ab.belabdia@gmail.com <ahmed@MacBook-Pro-de-Ahmed.local>
2024-10-23 12:53:30 +0200

    delete movie from list done

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,21 +1,40 @@
+import { useState } from "react";
 import movies from "../data/movies.json"
 
 //console.log(movies.length)
 
 function MovieList() {
+
+    const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+
+    const deleteMovie = (movieId) => {
+
+        const newArr = moviesToDisplay.filter((movieObj) => {
+            return movieObj.id !== movieId
+        })
+    
+        //moviesToDisplay = newArr; // nop ! do not modify the list like that
+
+        setMoviesToDisplay(newArr);
+
+    }
+    
+
+     
+
     return (
         <section className="MovieList">
         <h1>List of movies : </h1>
+        <h2>We currently have {moviesToDisplay.length} movies </h2>
 
-        {movies.map((movieObj)=>{
-            
+        {moviesToDisplay.map((movieObj)=>{ 
             return (
                 <div key = {movieObj.id} className="card">
                     <img src={movieObj.imgURL}/>
                     <h3>{movieObj.title}</h3>
                     <p>year : {movieObj.rating}</p>
                     <p>rating : {movieObj.rating}</p>
-
+                    <button onClick={() => {deleteMovie(movieObj.id)}}>Delete this movie</button>
                 </div>
             )
             
